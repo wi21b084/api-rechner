768b35816ceb4e2b71efe4dd2dbd0b76a874c034
wi21b084 <105796443+wi21b084@users.noreply.github.com>
2022-06-21 16:54:44 +0200

    Initial commit

---
+++ src/main/java/com/example/rechner/RechnerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rechner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RechnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RechnerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rechner/RechnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rechner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RechnerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
